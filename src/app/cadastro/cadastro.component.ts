8f9dec52c8cd94a78914e1e24ff3ccec4850425b
andisinha <andisinha39@gmail.com>
2024-05-20 16:43:13 -0300

    Formulário Finalizado

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { multicast } from 'rxjs';
+import { CustomValidator } from '../validator/custom-validator';
 
 @Component({
   selector: 'app-cadastro',
@@ -15,11 +16,15 @@ export class CadastroComponent {
   cadastroForm = this.formBuilder.group({
     nome : this.formBuilder.control('' ,[Validators.required, Validators.minLength(3), Validators.maxLength(35)]),
     email : this.formBuilder.control('' ,[Validators.email, Validators.required, Validators.maxLength(40)]),
-    usuario : this.formBuilder.control('', [Validators.required, Validators.maxLength(15)]),
-    senha : [null],
-    confirmarSenha : [null]
+    usuario : this.formBuilder.control('', [Validators.required, Validators.maxLength(15), CustomValidator.contemEspacos]),
+    senha : this.formBuilder.control('', [Validators.required, Validators.minLength(8), Validators.maxLength(16), Validators.pattern(/^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-zA-Z])/)]),
+    confirmarSenha : this.formBuilder.control('', [Validators.required])
 
-  });
+  }, {
+    validators: CustomValidator.mesmasSenhas('senha', 'confirmarSenha')
+  }
+
+);
 
   onEnviar(){
     console.table(this.cadastroForm.value);
